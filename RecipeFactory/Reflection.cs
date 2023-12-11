379d0331393865e80dc9ed89426d1d9969d3fe6e
Svyat Rozhkov <rozhkovsvyat@gmail.com>
2023-12-11 09:36:12 +0700

    Add GetPropertyValue method (Reflection.cs) Separation of GetTypeDescendants from Activate (Reflection.cs)

--- RecipeFactory/Reflection.cs
+++ RecipeFactory/Reflection.cs
@@ -6,14 +6,24 @@ namespace Tools;
 /// <summary>
 /// Contains methods for working with an assembly, modules, members, parameters and other objects in managed code.
 /// </summary>
-internal class Reflection
+internal static class Reflection
 {
+	/// <summary>
+	/// Returns all base type descendants within the specified assembly.
+	/// </summary>
+	/// <typeparam name="T">Base type.</typeparam>
+	/// <param name="assembly">Specified assembly.</param>
+	/// <returns>Array of type descendants.</returns>
+	internal static Type[] GetTypeDescendants<T>(Assembly assembly)
+		=> assembly.GetTypes().Where(t => 
+			(t.IsSubclassOf(typeof(T)) || typeof(T).IsAssignableFrom(t)) 
+			&& !t.IsAbstract).Select(t => t).ToArray();
+
 	/// <summary>
 	/// Calls a static constructor for all base type descendants within its assembly.    
 	/// </summary>
 	/// <typeparam name="T">Base type.</typeparam>
-	internal static void Activate<T>() 
-        => Activate<T>(typeof(T).Assembly);
+	internal static void Activate<T>() => Activate<T>(typeof(T).Assembly);
 
 	/// <summary>
 	/// Calls a static constructor for all base type descendants within the specified assembly.
@@ -21,10 +31,31 @@ internal class Reflection
 	/// <typeparam name="T">Base type.</typeparam>
 	/// <param name="assembly">Specified assembly.</param>
 	internal static void Activate<T>(Assembly assembly)
-    { 
-        var types = assembly.GetTypes().Where(t => 
-                (t.IsSubclassOf(typeof(T)) || typeof(T).IsAssignableFrom(t)) 
-                && !t.IsAbstract).Select(t => t).ToArray();
-        foreach (var type in types) RuntimeHelpers.RunClassConstructor(type.TypeHandle);
+    {
+	    foreach (var type in GetTypeDescendants<T>(assembly)) 
+		    RuntimeHelpers.RunClassConstructor(type.TypeHandle);
     }
+
+	/// <summary>
+	/// Gets the value of a specific type static property.
+	/// </summary>
+	/// <typeparam name="T">Return value type.</typeparam>
+	/// <param name="t">Current type.</param>
+	/// <param name="propertyName">Property name.</param>
+	/// <returns>Property value.</returns>
+	public static T? GetPropertyValue<T>(this Type t, string propertyName)
+	{
+		const BindingFlags flags = BindingFlags.Static | BindingFlags.Public;
+
+		var info = t.GetProperty(propertyName, flags);
+
+		if (info is not null) return info.GetValue(null, null) 
+				is { } infoValue ? (T)infoValue : default;
+
+		var fieldInfo = t.GetField(propertyName, flags);
+
+		return fieldInfo is null ? default : fieldInfo.GetValue(null) 
+			is { } fieldValue ? (T)fieldValue : default;
+
+	}
 }
